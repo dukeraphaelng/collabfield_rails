7eca24a93a4065f2848cfafee3d27d64dbd384e7
Duke <duke.raphael.ng@gmail.com>
2020-04-15 18:14:42 +1000

    Helpers: configure, create nav_helper, app_helper: include_all -> false, collapsible_elements -> split ino partials

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -5,6 +7,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+config.action_controller.include_all_helpers = false
 
 module Collabfield
   class Application < Rails::Application
